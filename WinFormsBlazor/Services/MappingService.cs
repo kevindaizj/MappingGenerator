bae32e3e5c907f45e4325e1c48ea79dfc30e1cb4
kevin <kevin@softbrain.com>
2023-02-07 18:05:46 +0800

    mapping property and refactoring

---
+++ WinFormsBlazor/Services/MappingService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsBlazor.Services
+{
+    public static class MappingService
+    {
+        public static string MapToInstace(string assemblyPath, string? fromTypeName, string toTypeName)
+        {
+            var isFromNull = string.IsNullOrWhiteSpace(fromTypeName);
+            var (fromProps, toProps) = GetFromToProperties(assemblyPath, fromTypeName, toTypeName);
+
+            var sb = new StringBuilder();
+            var nl = System.Environment.NewLine;
+            sb.Append($"new {toTypeName.Split(".").Last()} {nl}");
+            sb.Append("{");
+            sb.Append(nl);
+
+            foreach (var tp in toProps)
+            {
+                var name = tp.Name;
+                var fp = fromProps.FirstOrDefault(p => p.Name.ToUpper() == name.ToUpper());
+                var fpName = fp?.Name ?? (isFromNull ? name : string.Empty);
+
+                var comment = fp == null ? (isFromNull ? "" : "//") : "";
+
+                sb.Append($"    {comment}{name} = o.{fpName}, {nl}");
+            }
+
+            sb.Append("}");
+
+            return sb.ToString();
+        }
+
+        public static string MapToProperties(string assemblyPath, string? fromTypeName, string toTypeName)
+        {
+            var isFromNull = string.IsNullOrWhiteSpace(fromTypeName);
+            var (fromProps, toProps) = GetFromToProperties(assemblyPath, fromTypeName, toTypeName);
+
+            var fromVar = "from";
+            var toVar = "to";
+
+            var sb = new StringBuilder();
+            var nl = System.Environment.NewLine;
+
+            foreach (var tp in toProps)
+            {
+                var name = tp.Name;
+                var fp = fromProps.FirstOrDefault(p => p.Name.ToUpper() == name.ToUpper());
+                var fpName = fp?.Name ?? (isFromNull ? name : string.Empty);
+
+                var comment = fp == null ? (isFromNull ? "" : "//") : "";
+
+                sb.Append($"{comment}{toVar}.{name} = {fromVar}.{fpName}; {nl}");
+            }
+
+            return sb.ToString();
+        }
+
+        private static (List<PropertyInfo> from, List<PropertyInfo> to) GetFromToProperties(string assemblyPath, string? fromTypeName, string toTypeName)
+        {
+            var asm = Assembly.LoadFrom(assemblyPath);
+            var types = asm.GetTypes().Where(t => t.IsClass).ToList();
+
+            var fromType = fromTypeName != null ? types.SingleOrDefault(t => t.FullName == fromTypeName) : null;
+            var toType = types.SingleOrDefault(t => t.FullName == toTypeName);
+
+            var fromProps = fromType?.GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList() ?? new List<PropertyInfo>();
+            var toProps = toType?.GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList() ?? new List<PropertyInfo>();
+
+            return (fromProps, toProps);
+        }
+    }
+}
